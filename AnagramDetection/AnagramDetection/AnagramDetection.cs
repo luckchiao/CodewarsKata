d465781bbc7133c2d296e04fb4a6c52372248a52
chloe <luck_chiao@yahoo.com.tw>
2018-04-24 23:56:20 +0800

    add anagran detection

---
+++ AnagramDetection/AnagramDetection/AnagramDetection.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnagramDetection
+{
+    class AnagramDetection
+    {
+        static void Main(string[] args)
+        {
+            bool result = IsAnagram("foefet", "toffee");
+        }
+
+        public static bool IsAnagram(string test, string original)
+        {
+            char[] aTest = test.ToLower().ToArray();
+            Array.Sort(aTest);
+            char[] aOriginal = original.ToLower().ToArray();
+            Array.Sort(aOriginal);
+
+            return aTest.SequenceEqual(aOriginal);
+        }
+    }
+}
