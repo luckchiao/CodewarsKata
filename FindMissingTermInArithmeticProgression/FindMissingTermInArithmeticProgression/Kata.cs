8c1d6f3ea2b17f775a7aa3dd69dee3955170fbba
chloe <luck_chiao@yahoo.com.tw>
2018-05-13 12:58:09 +0800

    add miss term

---
+++ FindMissingTermInArithmeticProgression/FindMissingTermInArithmeticProgression/Kata.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindMissingTermInArithmeticProgression
+{
+    public class Kata
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public static int FindMissing(List<int> list)
+        {
+            List<int> temp = new List<int>();
+            for (int i = 0; i < list.Count; i++)
+            {
+                if (i > 0)
+                    temp.Add(list[i] - list[i - 1]);
+            }
+
+            int min = temp.Min();
+            int max = temp.Max();
+
+            var indexAtMax = temp.IndexOf(max);
+            return list[indexAtMax] + min;
+        }
+    }
+}
