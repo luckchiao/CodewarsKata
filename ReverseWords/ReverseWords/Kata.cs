6ba60c35d63f337438e0eccdb408794945a7cc73
chloe <luck_chiao@yahoo.com.tw>
2018-05-19 13:32:28 +0800

    add Reverse words

---
+++ ReverseWords/ReverseWords/Kata.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReverseWords
+{
+    public class Kata
+    {
+        public static string ReverseWords(string str)
+        {
+            var result = string.Empty;
+            var words = str.Split(' ');
+            for (var i = 0; i < words.Length; i++)
+            {
+                var temp = string.Empty;
+                foreach (var wChar in words[i].Reverse())
+                    temp += wChar.ToString();
+                result += temp;
+                if (i != words.Length - 1)
+                    result = result.Insert(result.Length, " ");
+            }
+
+            return result;
+        }
+    }
+}
---
+++ ReverseWords/ReverseWordsTests/KataTests.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ReverseWords;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReverseWords.Tests
+{
+    [TestClass()]
+    public class KataTests
+    {
+        [TestMethod()]
+        public void ReverseWords_First_Test()
+        {
+            var actual = Kata.ReverseWords("This is an example!");
+            Assert.AreEqual("sihT si na !elpmaxe", actual);
+        }
+
+        [TestMethod()]
+        public void ReverseWords_Second_Test()
+        {
+            var actual = Kata.ReverseWords("double  spaces");
+            Assert.AreEqual("elbuod  secaps", actual);
+        }
+    }
+}
