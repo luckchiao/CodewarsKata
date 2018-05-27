58617bd333132571e83dcbaec6430044f3f4a6a7
chloe <luck_chiao@yahoo.com.tw>
2018-05-27 17:49:12 +0800

    add HighestRankNumberInArray kata

---
+++ HighestRankNumberInArray/HighestRankNumberInArray/HighestRankNumberInArray.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HighestRankNumberInArray
+{
+    public class HighestRankNumberInArray
+    {
+        public static int HighestRank(int[] iarr)
+        {
+            var numbers = iarr.ToList();
+            return numbers.GroupBy(g => g).OrderByDescending(o => o.Count()).Select(s => s.Key).FirstOrDefault();
+        }
+    }
+}
---
+++ HighestRankNumberInArray/HighestRankNumberInArrayTests/HighestRankNumberInArrayTests.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using HighestRankNumberInArray;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HighestRankNumberInArray.Tests
+{
+    [TestClass()]
+    public class HighestRankNumberInArrayTests
+    {
+        [TestMethod()]
+        public void HighestRank_First_Case_Test()
+        {
+            var actual = HighestRankNumberInArray.HighestRank(new int[] { 12, 10, 8, 8, 3, 3, 3, 3, 2, 4, 10, 12, 10 });
+            Assert.AreEqual(3, actual);
+        }
+
+        [TestMethod()]
+        public void HighestRank_Second_Case_Test()
+        {
+            var actual = HighestRankNumberInArray.HighestRank(new int[] { 12, 10, 8, 12, 7, 6, 4, 10, 12, 10 });
+            Assert.AreEqual(12, actual);
+        }
+    }
+}
